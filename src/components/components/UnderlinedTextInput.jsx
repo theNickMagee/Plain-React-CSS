105453fbcf6d761e909b2585a97711a5f6d15385
theNickMagee <nick.c.magee13@gmail.com>
2021-11-29 10:07:38 -0600

    front end work

---
+++ src/components/components/UnderlinedTextInput.jsx
@@ -0,0 +1,64 @@
+import React, { useState } from "react";
+import "./UnderlinedTextInput.css";
+
+import { AiFillEye } from "react-icons/ai";
+import { AiFillEyeInvisible } from "react-icons/ai";
+
+const UnderlinedTextInput = ({
+  label,
+  isInvalid,
+  value,
+  onValueChange,
+  type,
+}) => {
+  const [isActive, setIsActive] = useState(false);
+  const [passwordVisible, setPasswordVisible] = useState(false);
+  const [textType, setTextType] = useState(type || "text");
+
+  const togglePasswordIcon = () => {
+    if (!passwordVisible) {
+      setTextType("text");
+    } else {
+      setTextType("password");
+    }
+    setPasswordVisible(!passwordVisible);
+  };
+  return (
+    <div
+      className={`label-input-container ${isInvalid && "invalid"} ${
+        isActive && "active"
+      }`}
+    >
+      <div className="field-label">{label}</div>
+      <div className="input-container">
+        {/* <MdEmail className="form-icon" size={20} style={{}} /> */}
+        <input
+          className={`form-input ${isInvalid ? "invalid" : ""}`}
+          value={value}
+          placeholder="Enter your email here"
+          onChange={onValueChange}
+          onBlur={() => setIsActive(false)}
+          onFocus={() => setIsActive(true)}
+          type={textType}
+        />
+        {type === "password" &&
+          (!passwordVisible ? (
+            <AiFillEyeInvisible
+              className={"password-icon"}
+              size={20}
+              onClick={togglePasswordIcon}
+            />
+          ) : (
+            <AiFillEye
+              className="password-icon"
+              size={20}
+              onClick={togglePasswordIcon}
+            />
+          ))}
+      </div>
+      {/* <span className="input-border" /> */}
+    </div>
+  );
+};
+
+export default UnderlinedTextInput;
--- src/components/signup/SignUp.jsx
+++ src/components/signup/SignUp.jsx
@@ -1,14 +1,24 @@
-import React, { useState } from "react";
-import { MdEmail } from "react-icons/md";
-import { RiLockPasswordFill } from "react-icons/ri";
-import OtherSignInOptions from "../signInOptions/OtherSignInOptions";
+import React, { useEffect, useState } from "react";
+import "../loginAndSignUp/LoginAndSignUp.css";
 
-// import "./Login.css";
+import { isEmail } from "validator";
+
+import OtherSignInOptions from "../signInOptions/OtherSignInOptions";
+import AuthService from "../../AuthService";
+import UnderlinedTextInput from "../components/UnderlinedTextInput";
 
 const SignUp = (props) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [repeatPassword, setRepeatPassword] = useState("");
+
+  const [emailInvalid, setEmailInvalid] = useState(false);
+  const [passwordInvalid, setPasswordInvalid] = useState(false);
+  const [invalidMessage, setInvalidMessage] = useState("");
+
+  //passwords visibility
+  const [passwordShow, setPasswordShow] = useState(false);
+  const [repeatPasswordShow, setRepeatPasswordShow] = useState(false);
 
   const handleEmailChange = (e) => {
     setEmail(e.target.value);
@@ -21,47 +31,100 @@ const SignUp = (props) => {
     setRepeatPassword(e.target.value);
   };
 
+  const handleSubmit = () => {
+    if (checkEmail()) {
+      setEmailInvalid(true);
+      setInvalidMessage(checkEmail());
+      return;
+    }
+    if (checkPassword()) {
+      setPasswordInvalid(true);
+      setInvalidMessage(checkPassword());
+      return;
+    }
+    if (checkPasswordMatch()) {
+      setInvalidMessage(checkPasswordMatch());
+      return;
+    }
+    AuthService.register(email, password)
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((e) => {
+        console.log("error registering: ", e);
+      });
+  };
+
+  const checkEmail = () => {
+    if (!isEmail(email)) {
+      return "Email must be a valid address.";
+    } else {
+      return false;
+    }
+  };
+
+  const checkPassword = () => {
+    if (password.length < 6 || password.length > 40) {
+      return "The password must be between 6 and 40 characters.";
+    } else {
+      return false;
+    }
+  };
+
+  const checkPasswordMatch = () => {
+    if (password !== repeatPassword) {
+      return "Passwords must match.";
+    } else {
+      return false;
+    }
+  };
+
+  const togglePasswordShow = () => {
+    setPasswordShow(!passwordShow);
+  };
+
+  const toggleRepeatPasswordShow = () => {
+    setRepeatPasswordShow(!repeatPasswordShow);
+  };
+
+  useEffect(() => {
+    setEmailInvalid(false);
+    setPasswordInvalid(false);
+  }, [email, password, repeatPassword]);
+
   return (
     <div className="login-signup-container">
-      <div className="label-input-container">
-        <div className="field-label">Email</div>
-        {/* <MdEmail className="form-icon" size={20} style={{}} /> */}
-        <input
-          className="form-input"
-          type="text"
-          value={email}
-          placeholder="Enter your email here"
-          onChange={handleEmailChange}
-        />
-        {/* <span className="input-border" /> */}
-      </div>
+      <UnderlinedTextInput
+        label={"Email"}
+        isInvalid={emailInvalid}
+        value={email}
+        onValueChange={handleEmailChange}
+      />
 
-      <div className="label-input-container">
-        <div className="field-label">Password</div>
-        {/* <RiLockPasswordFill className="form-icon" size={20} style={{}} /> */}
-        <input
-          className="form-input"
-          type="password"
-          placeholder="Enter your password here"
-          value={password}
-          onChange={handlePasswordChange}
-        />
-        {/* <span className="input-border" /> */}
-      </div>
-      <div className="label-input-container">
-        <div className="field-label">Repeat Password</div>
-        {/* <RiLockPasswordFill className="form-icon" size={20} style={{}} /> */}
-        <input
-          className="form-input"
-          type="password"
-          placeholder="Repeat your password"
-          value={repeatPassword}
-          onChange={handleRepeatPasswordChange}
-        />
-        {/* <span className="input-border"></span> */}
-      </div>
+      <UnderlinedTextInput
+        label={"Password"}
+        isInvalid={passwordInvalid}
+        value={password}
+        onValueChange={handlePasswordChange}
+        type={"password"}
+      />
+
+      <UnderlinedTextInput
+        label={"Repeat Password"}
+        isInvalid={passwordInvalid}
+        value={repeatPassword}
+        onValueChange={handleRepeatPasswordChange}
+        type={"password"}
+      />
+
+      {invalidMessage && invalidMessage.length > 1 && (
+        <div className="invalid-message-container">{invalidMessage}</div>
+      )}
+
       <div className="login-button-container">
-        <button className="login-button">Start Your Free Trial</button>
+        <button className="login-button" onClick={handleSubmit}>
+          Start Your Free Trial
+        </button>
       </div>
       <div className="or-container">Or</div>
       <OtherSignInOptions signIn={false} />
