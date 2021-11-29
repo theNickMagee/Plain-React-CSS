34b1a49e69e1ffed5c9b176afc5154b09b146a4e
theNickMagee <nick.c.magee13@gmail.com>
2021-11-29 10:26:05 -0600

    login validation front end

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { MdEmail } from "react-icons/md";
 import { RiLockPasswordFill } from "react-icons/ri";
 import OtherSignInOptions from "../signInOptions/OtherSignInOptions";
@@ -9,6 +9,10 @@ const Login = (props) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
+  const [emailInvalid, setEmailInvalid] = useState(false);
+  const [passwordInvalid, setPasswordInvalid] = useState(false);
+  const [invalidMessage, setInvalidMessage] = useState("");
+
   const handleEmailChange = (e) => {
     setEmail(e.target.value);
   };
@@ -16,27 +20,67 @@ const Login = (props) => {
     setPassword(e.target.value);
   };
 
+  const submit = () => {
+    if (checkEmail()) {
+      setEmailInvalid(true);
+      setInvalidMessage(checkEmail());
+      return;
+    }
+    if (checkPassword()) {
+      setPasswordInvalid(true);
+      setInvalidMessage(checkPassword());
+      return;
+    }
+  };
+
+  const checkEmail = () => {
+    if (!(email && email.length > 1)) {
+      return "Email is required.";
+    } else {
+      return false;
+    }
+  };
+
+  const checkPassword = () => {
+    if (!(password && password.length > 1)) {
+      return "A password is required.";
+    } else {
+      return false;
+    }
+  };
+
+  useEffect(() => {
+    setEmailInvalid(false);
+    setPasswordInvalid(false);
+  }, [email, password]);
+
   return (
     <div className="login-signup-container">
       <UnderlinedTextInput
         label={"Email"}
-        isInvalid={false}
         value={email}
         onValueChange={handleEmailChange}
         placeholder="Enter your email here"
+        isInvalid={emailInvalid}
       />
 
       <UnderlinedTextInput
         label={"Password"}
-        isInvalid={false}
+        isInvalid={passwordInvalid}
         value={password}
         onValueChange={handlePasswordChange}
         type={"password"}
         placeholder="Enter your password here"
       />
 
+      {invalidMessage && invalidMessage.length > 1 && (
+        <div className="invalid-message-container">{invalidMessage}</div>
+      )}
+
       <div className="login-button-container">
-        <button className="login-button">Log In</button>
+        <button className="login-button" onClick={submit}>
+          Log In
+        </button>
         <div className="forgot-password-click">Forgot Your Password? </div>
       </div>
       {/* <div className="forgot-password-container"></div> */}
